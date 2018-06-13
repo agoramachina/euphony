92ae373fca52a86fb6d35ad6a08eebe4c198ece0
agoramachina <agoramachina@gmail.com>
2018-06-13 19:29:46 -0400

    undo

--- src/compiled/Scene.js
+++ src/compiled/Scene.js
@@ -12,7 +12,7 @@
       width = $container.width();
       height = $container.height();
       scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(0, width / height, 0.001, 100000);
+      camera = new THREE.PerspectiveCamera(60, width / height, 0.001, 100000);
       camera.lookAt(new THREE.Vector3());
       scene.add(camera);
       renderer = new THREE.WebGLRenderer({
